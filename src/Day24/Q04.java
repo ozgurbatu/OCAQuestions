f676e80f7c2cd0ffabffa9ea3aa4d872b710b8e1
Ozgur <ozgurbatu14@gmail.com>
2022-04-02 03:50:39 +0300

    day24

---
+++ src/Day24/Q04.java
@@ -0,0 +1,59 @@
+package Day24;
+
+public class Q04 {
+    public static void main(String[] args) {
+
+        int [] [] arr = new int [2][4]; // 2 katli her katta 4 daire
+        arr[0] = new int [] {1, 3, 5, 7,}; // 0. katta 4 daire var
+        arr [1] = new int [] {1, 3}; // 1.katta 2 daire var
+        for (int[] a : arr) {
+            for (int i : a){
+                System.out.print(i + " "); // 1 3 5 7
+                                            // 1 3
+
+            }
+            System.out.println();
+        }
+    }
+}
+/**
+ What is the result?
+ A. Compilation fails
+ B. 1 3
+ 1 3
+ C. 1 3
+ Followed by an ArrayIndexOutOfBoundsException
+ D. 1 3
+ 1 3 0 0
+ E. 1 3 5 7
+ 1 3
+
+ A. Option A
+ B. Option B
+ C. Option C
+ D. Option D
+ E. Option E
+ Answer: E
+
+ Explanation:
+ Your code:
+
+ public class Q17 {
+ public static void main(String[] args) {
+
+ int [] [] arr = new int [2][4];
+ arr[0] = new int [] {1, 3, 7};
+ arr [1] = new int [] {1, 3};
+ for (int[] a : arr) {
+ for (int i : a){
+ System.out.println(i + " ");
+
+ }
+ System.out.println();
+ }
+ }
+ }
+
+ output: 1 3 5 7
+ 1 3
+ */
